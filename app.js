1f673cec592fc22fb7d756c54a2e270109769229
ashutosh2329 <sonutiwari2329@gmail.com>
2020-08-09 00:57:36 +0530

    bought css styles

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.set('view engine', 'ejs');
-let items = [];
+let items = ["Eat","Sleep","Cook",];
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
 	let options = {
