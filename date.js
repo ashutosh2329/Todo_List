f21ec16895aef902ffcf9e846617dc956a0d1694
ashutosh2329 <sonutiwari2329@gmail.com>
2020-08-20 23:35:38 +0530

    changes

--- date.js
+++ date.js
@@ -1,24 +1,19 @@
 
-module.exports.getDate = getDate;
-module.exports.getDay = getDay;
-
-function getDate(){
+module.exports.getDate = function(){
     let today = new Date();
 	let options = {
 		weekday: 'long',
 		day: 'numeric',
 		month: 'long'
 	};
-	let day = today.toLocaleDateString('en-us', options); 
-	return day;
+	return(today.toLocaleDateString('en-us', options)); 
 }
 
-
-function getDay(){
+module.exports.getDay = function(){
     let today = new Date();
 	let options = {
 		weekday: 'long'
 	};
-	let day = today.toLocaleDateString('en-us', options); 
-	return day;
+	return(today.toLocaleDateString('en-us', options)); 
+	
 }
